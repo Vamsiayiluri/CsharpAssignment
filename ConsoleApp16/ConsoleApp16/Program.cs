f65f66c494e087b1814cf4c56f816e6eb7772940
VamsiAyiluri <vamsi.ayiluri@cognine.com>
2023-05-24 13:07:19 +0530

    Updated Assignment

--- ConsoleApp16/ConsoleApp16/Program.cs
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -1,35 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp16
 {
-    internal class Program
+    public class FindSubstring
     {
-        static void Main(string[] args)
+        public int subStringCount(string s1, string s2, ref List<int> indexes)
         {
-            string S1 = "abcdabcabd";
-            string S2 = "lb";
-            List<int> indexes = new List<int>();
-
             int count = 0;
-            for (int i = 0; i <= S1.Length - S2.Length; i++)
+
+            for (int i = 0; i <= s1.Length - s2.Length; i++)
             {
-                if (S1.Substring(i, S2.Length) == S2)
+                if (s1.Substring(i, s2.Length) == s2)
                 {
                     count++;
                     indexes.Add(i);
-
                 }
             }
+
+            return count;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string 1");
+            string string1 = Console.ReadLine();
+            Console.WriteLine("Enter string 2");
+            string string2 = Console.ReadLine();
+            List<int> indexes = new List<int>();
+
+            FindSubstring findSubstring = new FindSubstring();
+            int count = findSubstring.subStringCount(string1, string2, ref indexes);
+
             Console.WriteLine($"Number of times occurred: {count}");
             Console.WriteLine($"Index Positions: ");
+
             if (indexes.Count == 0)
             {
                 Console.Write("NULL");
             }
+
             else
             {
                 for (int i = 0; i < indexes.Count; i++)
@@ -37,7 +49,6 @@ namespace ConsoleApp16
                     Console.Write(indexes[i] + " ");
                 }
             }
-
         }
     }
 }
