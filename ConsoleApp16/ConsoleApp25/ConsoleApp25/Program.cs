dddd38179b70a03c6f1eefe033f426f3887c35ee
VamsiAyiluri <vamsi.ayiluri@cognine.com>
2023-07-20 19:40:55 +0530

    klk

---
+++ ConsoleApp16/ConsoleApp25/ConsoleApp25/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract int GetArea();
+
+    public int GetShape()
+    {
+        Console.WriteLine("helloo bye");
+        return 0;
+    }
+}
+
+class Square : Shape
+{
+    private int _side;
+    public override int GetArea() => _side * _side;
+    public int GetShape()
+    {
+        Console.WriteLine("helloo 123456789bye");
+        return 0;
+    }
+
+    static void Main()
+    {
+        Square sq = new Square();
+        Console.WriteLine($"Area of the square = {sq.GetArea()}");
+        sq.GetShape();
+    }
+}
