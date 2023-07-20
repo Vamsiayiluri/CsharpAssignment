63421f90038f12a00ccef843d3ac733d3dc784e9
VamsiAyiluri <vamsi.ayiluri@cognine.com>
2023-07-20 19:28:55 +0530

    vamsi

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using BusinessModels;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// This class will call the signup,login,forgot password methods on their approprite call infinite times unless user breaks the loop.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The enum stores what are the inputs to be given
+        /// </summary>
+        public enum Input
+        {
+            Signup = 1,
+            Login = 2,
+            ForgotPassword = 3,
+            Exit = 4
+        }
+        /// <summary>
+        /// The main method will create objects and validate the input and call the appropriate login,signup,forgot password methods
+        /// </summary>
+        public static void Main()
+        {
+            while (true)
+            {
+                InputOutput inputOutput = new InputOutput();
+                inputOutput.Display(Literals.selectInput);
+
+                string input = inputOutput.ReadInput();
+
+                Authentication authObj = new Authentication();
+                authObj = new Authentication();
+                if (Enum.TryParse<Input>(input, out var inputValue))
+                {
+                    switch (inputValue)
+                    {
+                        case Input.Signup:
+                            authObj.Signup();
+                            break;
+                        case Input.Login:
+                            authObj.Login();
+                            break;
+                        case Input.ForgotPassword:
+                            authObj.ForgotPassword();
+                            break;
+                        case Input.Exit:
+                            authObj.Exit();
+                            break;
+                        default:
+                            inputOutput.Display(Literals.validOption);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp/DataLayer/DALAuthentication.cs
@@ -0,0 +1,81 @@
+﻿using BusinessModels;
+using System;
+
+namespace DataLayer
+{
+    /// <summary>
+    /// this class stores data of the users in a list of lists amd performs signup and login operations
+    /// </summary>
+    public class DALAuthentication : IDALAuthentication
+    {
+        /// <summary>
+        /// this method adds data of the user into the list while signing up
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public void AddData(User userObj)
+        {
+            DataSource.data.Add(new User { Username = userObj.Username, Password = userObj.Password, Email = userObj.Email });
+        }
+
+        /// <summary>
+        /// This method checks whether data entered by user while logging in is correct or not by checking in the list
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool ValidateLogin(User userObj)
+        {
+            User user = DataSource.data.Find(user => user.Username == userObj.Username);
+            if (user != null && user.Password == userObj.Password)
+            {
+                return true;
+            }
+            return false;           
+        }
+        /// <summary>
+        /// This method will validate whether the given details are valid or not
+        /// </summary>
+        /// <param name="newUser"></param>
+        /// <returns></returns>
+        public bool ValidateUser(User userObj)
+        {
+            User user = DataSource.data.Find(user => user.Username == userObj.Username);
+            if (user != null && user.Email == userObj.Email)
+            {
+                return true;
+            }
+            return false;           
+        }
+        /// <summary>
+        /// This method will update the new password in the users data
+        /// </summary>
+        /// <param name="newUser"></param>
+        /// <returns></returns>
+        public void UpdatePassword(User userObj)
+        {
+            User user = DataSource.data.Find(user => user.Username == userObj.Username);
+            if (user != null )
+            {
+                user.Password = userObj.Password;
+            }                      
+        }
+
+        /// <summary>
+        /// this method validates whether the username entered is valid or not
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <returns></returns>
+        public bool ValidateUsername(string Username)
+        {
+            User user = DataSource.data.Find(user => user.Username == Username);
+            if (user != null )
+            {
+                return false;
+            }
+            return true;           
+        }
+    }
+}
